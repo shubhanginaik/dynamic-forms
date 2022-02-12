faf9f3641a1ca4f10f5b055ce34eb1ccb40fcf2c
Shubhangi <shubhanginaik@BC-macair029s-MacBook-Air.local>
2022-02-12 14:53:06 +0200

    added formfields state and mapped

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # How to build Dynamic Forms in React
-## First create a form in react
+## First create a form in react(static)
+
--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [inputfields, setInputfields]= useState([
+    {name:'',age:''}
+  ])
   return (
     <div className="App">
-      <h1h>Hi</h1h>
+      <form>
+        {inputfields.map((input,index)=>{
+          return(
+        <div key={index}>
+          <input
+          name="name"
+          placeholder="Name"
+
+          />
+          <input
+          name="age"
+          placeholder="Age"
+          
+          />
+        </div>
+          )
+        })
+        }
+      </form>
     </div>
   );
 }
